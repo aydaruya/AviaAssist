069caf2dc9b76b5ce4bb084e310e9e89f8d9a5b2
Ayda <aarslan@skidmore.edu>
2023-12-04 13:07:07 -0500

    Initial Commit

---
+++ AviaAssist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AviaAssist
+//
+//  Created by Ayda :) on 11/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
