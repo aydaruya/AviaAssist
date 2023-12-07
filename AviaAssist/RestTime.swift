b2e09f327c0c62156ce35d9389361feb21849ffb
Ayda <aarslan@skidmore.edu>
2023-12-07 13:26:47 -0500

    fixed scroll and started resting time calc

---
+++ AviaAssist/RestTime.swift
@@ -0,0 +1,47 @@
+//
+//  RestTime.swift
+//  AviaAssist
+//
+//  Created by Ayda :) on 12/7/23.
+//
+
+import UIKit
+
+class RestingTimeCalculatorViewController: UIViewController {
+    
+    @IBOutlet weak var numPilotsTextField: UITextField!
+    @IBOutlet weak var hoursTextField: UITextField!
+    @IBOutlet weak var minutesTextField: UITextField!
+    @IBOutlet weak var outputLabel: UILabel!
+    @IBOutlet weak var submitButton: UIButton!
+    
+    @objc func submit() {
+        // Check if all fields have valid input
+        guard let numberOfPilotsText = numPilotsTextField.text,
+              let numberOfPilots = Int(numberOfPilotsText),
+              let hoursText = hoursTextField.text,
+              let hours = Int(hoursText),
+              let minutesText = minutesTextField.text,
+              let minutes = Int(minutesText) else {
+            outputLabel.text = "Invalid input. Please enter valid numbers."
+            return
+        }
+        
+        // total flight duration in minutes
+        let totalFlightDuration = hours * 60 + minutes
+        
+        // flight time per pilot
+        let flightTimePerPilot = Double(totalFlightDuration) / Double(numberOfPilots)
+        
+        // result
+        outputLabel.text = "Each pilot will fly for \(flightTimePerPilot) minutes."
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Connect the submitButton to the submit function
+        submitButton.addTarget(self, action: #selector(submit), for: .touchUpInside)
+    }
+}
+    
--- AviaAssist/ViewController.swift
+++ AviaAssist/ViewController.swift
@@ -8,11 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var scriptPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        scriptPicker.delegate = self
+        scriptPicker.dataSource = self
     }
-
-
+    
 }
+
+        extension ViewController: UIPickerViewDataSource {
+            func numberOfComponents(in pickerView: UIPickerView) -> Int {
+                return 1
+            }
+            
+            func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+                return 10
+            }
+        }
+        extension ViewController: UIPickerViewDelegate {
+            func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+                return "test"
+            }
+        }
+        
+        
+        
+        
+        
+        //   var pickerData = ["Weather Conditions", "Deicing or Delays", "Turbulance", "Introduction", "Emergency Procedures", "Change of Flight Path", "Special Services", "Arrivals and Customs"]
+        
+        
+        
+        
+        // Do any additional setup after loading the view.
+    
 
